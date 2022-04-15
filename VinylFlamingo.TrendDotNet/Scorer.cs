0bbf13bd21b0e7aaba8d198eef610d0f1d524ccf
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2022-04-15 17:31:08 -0400

    Add project files.

---
+++ VinylFlamingo.TrendDotNet/Scorer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylFlamingo.TrendDotNet
+{
+    public class Scorer
+    {
+        public List<Factor> Factors = new List<Factor>();
+        public double Gravity = 0.2;
+
+        /// <summary>
+        /// Calculate a score using the added Factors, and the time in days since the content was published.
+        /// </summary>
+        /// <param name="daysSincePost">Days since the content has been published.</param>
+        /// <returns>A new score of type double</returns>
+        /// <exception cref="Exception">No factors found.</exception>
+        public double Calculate(double daysSincePost)
+        {
+            if (Factors != null )
+            {
+                double aggregatedWeightedHits = 0;
+                foreach (Factor factor in Factors)
+                {
+                    double weightedHit = factor.Hits * factor.Weight;
+                    aggregatedWeightedHits = aggregatedWeightedHits + weightedHit;
+                }
+
+                double timeValue = daysSincePost * Gravity;
+
+                double score = aggregatedWeightedHits / timeValue;
+                return score;
+            }
+            else
+            {
+                throw new Exception("No Factors were found to consider in scoring. Please add factors using Scorer.AddFactor(name, hits, weight)");
+            }
+
+        }
+        
+        /// <summary>
+        /// Calculate a score using the added Factors, and the time in days since the content was published.
+        /// </summary>
+        /// <param name="daysSincePost">Days since the content has been published.</param>
+        /// <returns>A new score of type int</returns>
+        /// <exception cref="Exception">No factors found.</exception>
+        public int Calculate(int daysSincePost)
+        {
+            var _daysSincePost = Convert.ToDouble(daysSincePost);
+            var score = Calculate(_daysSincePost);
+            var convertedScore = Convert.ToInt32(score);
+            return convertedScore;
+        }
+
+        /// <summary>
+        /// Add a new factor to be considered in scoring. 
+        /// </summary>
+        /// <param name="name">The name of the factor ex: "page views"</param>
+        /// <param name="hits">The quantity this factor has</param>
+        /// <param name="weight">How strongly should the scorer consider this factor.</param>
+        public void AddFactor(string name, double hits, double weight)
+        {
+            Factor factor = new Factor()
+            {
+                Name = name,
+                Hits = hits,
+                Weight = weight
+            };
+
+            Factors.Add(factor);
+        }
+
+
+
+
+
+
+    }
+    public class Factor
+    {
+        public string Name { get; set; } = "generic";
+        public double Hits { get; set; } = 0;
+        public double Weight { get; set; } = 1;
+
+
+    }
+}
---
+++ src/console/console/Program.cs
@@ -0,0 +1,21 @@
+﻿using VinylFlamingo.TrendDotNet;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        var trendService = new Scorer();
+
+        trendService.AddFactor("page views", 1700202002, 1);
+
+        foreach(var factor in trendService.Factors)
+        {
+            Console.WriteLine(factor.Name.ToString());
+        }
+
+        Console.WriteLine(trendService.Calculate(45));
+    }
+    
+
+}
+
